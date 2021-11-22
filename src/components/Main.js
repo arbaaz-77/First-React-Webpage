934a7413e27451aec24a684aaef8ee1053d8b237
Arbaaz Sumar <43441056+arbaaz-77@users.noreply.github.com>
2021-11-22 17:33:11 +0300

    react code

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first releasedin 2013</li>
+                <li>Was orginally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps</li>
+            </ul>
+        </main>
+    )
+}
+
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '/Users/hp/Desktop/REACT/firstreactapp/src/logo192.png'
+
+function Navbar() {
+    return (
+        <nav>
+            <img src={logo} alt="logo" />
+            <h2>ReactFacts</h2>
+            <h3>React Course - Project 1</h3>
+        </nav>
+    )
+}
+
+export default Navbar
